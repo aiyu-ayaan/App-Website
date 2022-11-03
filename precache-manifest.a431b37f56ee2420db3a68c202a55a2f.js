c7b9bace7e4854ab45e0422cbb5df92e24e4788c
aiyu-ayaan <aiyuayaan22@gmail.com>
2022-11-03 18:49:57 +0530

    Updates

---
+++ precache-manifest.a431b37f56ee2420db3a68c202a55a2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a359e544e65e42509c41",
+    "url": "/App-Website/static/css/main.8ac75438.chunk.css"
+  },
+  {
+    "revision": "a359e544e65e42509c41",
+    "url": "/App-Website/static/js/main.a359e544.chunk.js"
+  },
+  {
+    "revision": "6a4458e44d446b5798d7",
+    "url": "/App-Website/static/js/1.6a4458e4.chunk.js"
+  },
+  {
+    "revision": "80735f429d5401e2e1a7",
+    "url": "/App-Website/static/js/runtime~main.80735f42.js"
+  },
+  {
+    "revision": "85dfecd28f13cf756887b4ed9a5f138c",
+    "url": "/App-Website/static/media/app.85dfecd2.png"
+  },
+  {
+    "revision": "20d66edce8933256586e6b679a1225f3",
+    "url": "/App-Website/index.html"
+  }
+];
