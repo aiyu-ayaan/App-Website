369fdbfd730569f7379843e15f0447c769880e23
Ayaan <aiyuayaan22@gmail.com>
2023-01-04 18:22:53 +0530

    Updates

--- precache-manifest.34758c18b1ffd2afe24256fc2b8918fa.js
+++ precache-manifest.34758c18b1ffd2afe24256fc2b8918fa.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "a359e544e65e42509c41",
-    "url": "/App-Website/static/css/main.8ac75438.chunk.css"
+    "revision": "948e726b86f9e53de0f6",
+    "url": "/App-Website/static/css/main.23e69cf2.chunk.css"
   },
   {
-    "revision": "a359e544e65e42509c41",
-    "url": "/App-Website/static/js/main.a359e544.chunk.js"
+    "revision": "948e726b86f9e53de0f6",
+    "url": "/App-Website/static/js/main.948e726b.chunk.js"
   },
   {
     "revision": "6a4458e44d446b5798d7",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/App-Website/static/media/app.85dfecd2.png"
   },
   {
-    "revision": "2dad183a0527a6a2404eb8d96e8c0c6c",
+    "revision": "260f20cc6ce89112a908cbed46809b5e",
     "url": "/App-Website/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/App-Website/precache-manifest.70302f5c60d4e5058bc6f29ff933aaeb.js"
+  "/App-Website/precache-manifest.34758c18b1ffd2afe24256fc2b8918fa.js"
 );
 
 workbox.clientsClaim();
